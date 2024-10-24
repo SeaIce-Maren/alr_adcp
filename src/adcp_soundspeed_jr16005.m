04ba852cc851f6d719fd85e9ccd52730fe1de70c
SeaIce-Maren <marenrichter@gmx.net>
2024-10-24 09:31:56 +0100

    adapted to run with TARSAN data

--- src/adcp_soundspeed_jr16005.m
+++ src/adcp_soundspeed_jr16005.m
@@ -10,23 +10,32 @@
 % Calls f_correct_soundspeed (robust_interp1 and expects Gibbs Seawater
 % toolbox on the path)
 
+
+rootp = split(pwd,filesep);
+
+ALRdatadir = fullfile(rootp{1:end-2},'ALR','ALR_data');%[dataraw,'alrdata/'];
+datadir = fullfile(rootp{1:end-2},'alr_adcp','adcp_data');
+
+% add location of toolbpoxes to matlab path
+addpath(genpath(fullfile(rootp{1:end-2},'toolboxes')));
+
 % Filenaming conventions
 filein_suffix = '_adcp_timestamps2.mat';
 fileout_suffix = '_adcp_soundspeed3.mat';
 
 %% Process M41
-mnumstr='41';
+mnumstr='M137_M138';
 % Load data files
-load([dataint,'alrctd',mnumstr,'.mat'],'alrctd');
-load([dataint,'M',mnumstr,filein_suffix]);
+load(fullfile(datadir,['alrctd_',mnumstr,'.mat']),'alrctd');
+load(fullfile(datadir,[mnumstr,filein_suffix]));
 
 % Apply a sound speed correction 
 [alladcp_dn,alladcp_up] = f_correct_soundspeed(alladcp_dn,alladcp_up,alrctd);
 
 % Save result
-disp(['adcp_soundspeed: Saving results to ',dataint,'M',mnumstr,fileout_suffix])
-save([dataint,'M',mnumstr,fileout_suffix],'alladcp_dn','alladcp_up');
-
+disp(['adcp_soundspeed: Saving results to ',datadir,mnumstr,fileout_suffix])
+save(fullfile(datadir,[mnumstr,fileout_suffix]),'alladcp_dn','alladcp_up');
+return
 %% Process M42
 mnumstr='42';
 % Load data files
